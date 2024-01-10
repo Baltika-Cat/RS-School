24c7578021a4ed3a45e6dcae87228e550e34918f
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-01-10 05:04:36 +0300

    feat: add hangman's body and head to project

--- hangman/script.js
+++ hangman/script.js
@@ -1,11 +1,21 @@
 const body = document.querySelector('body');
 
+const gallowsWrapper = document.createElement('div');
+body.appendChild(gallowsWrapper);
+gallowsWrapper.classList.add('gallowsWrapper');
 const gallows = document.createElement('img');
 gallows.src = 'images/gallows.svg';
-body.appendChild(gallows);
+gallowsWrapper.appendChild(gallows);
 gallows.classList.add('gallows');
 
-const head = document.createElement('img');
-head.src = 'images/head.svg';
-head.classList.add('invisible');
-gallows.appendChild(head);
+const hangHead = document.createElement('img');
+hangHead.src = 'images/head.svg';
+hangHead.classList.add('hangHead');
+hangHead.classList.add('invisible');
+gallowsWrapper.appendChild(hangHead);
+
+const hangBody = document.createElement('img');
+hangBody.src = 'images/body.svg';
+hangBody.classList.add('hangBody');
+hangBody.classList.add('invisible');
+gallowsWrapper.appendChild(hangBody);
