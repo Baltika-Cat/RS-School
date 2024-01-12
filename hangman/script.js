96ca51ade52d47d55f7386ffbd096995bed3a369
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-01-12 23:28:11 +0400

    feat: add keyboard input and screen keyboard onclick input

--- hangman/script.js
+++ hangman/script.js
@@ -5,56 +5,70 @@ body.classList.add('body');
 
 const main = document.createElement('main');
 main.classList.add('main');
-body.appendChild(main);
+body.append(main);
 
 const gallowsWrapper = document.createElement('div');
-main.appendChild(gallowsWrapper);
+main.append(gallowsWrapper);
 gallowsWrapper.classList.add('gallows-wrapper');
+
+const imagesVisible = [];
 
 const gallows = document.createElement('img');
 gallows.src = 'images/gallows.svg';
-gallowsWrapper.appendChild(gallows);
+gallowsWrapper.append(gallows);
 gallows.classList.add('gallows');
+imagesVisible.push(gallows);
 
 const hangHead = document.createElement('img');
 hangHead.src = 'images/head.svg';
 hangHead.classList.add('hang-head');
 hangHead.classList.add('invisible');
-gallowsWrapper.appendChild(hangHead);
+gallowsWrapper.append(hangHead);
+imagesVisible.push(hangHead);
 
 const hangBody = document.createElement('img');
 hangBody.src = 'images/body.svg';
 hangBody.classList.add('hang-body');
 hangBody.classList.add('invisible');
-gallowsWrapper.appendChild(hangBody);
+gallowsWrapper.append(hangBody);
+imagesVisible.push(hangBody);
 
 const hangLeftHand = document.createElement('img');
 hangLeftHand.src = 'images/left-hand.svg';
 hangLeftHand.classList.add('hang-left-hand');
 hangLeftHand.classList.add('invisible');
-gallowsWrapper.appendChild(hangLeftHand);
+gallowsWrapper.append(hangLeftHand);
+imagesVisible.push(hangLeftHand);
 
 const hangRightHand = document.createElement('img');
 hangRightHand.src = 'images/right-hand.svg';
 hangRightHand.classList.add('hang-right-hand');
 hangRightHand.classList.add('invisible');
-gallowsWrapper.appendChild(hangRightHand);
+gallowsWrapper.append(hangRightHand);
+imagesVisible.push(hangRightHand);
 
 const hangLeftLeg = document.createElement('img');
 hangLeftLeg.src = 'images/left-leg.svg';
 hangLeftLeg.classList.add('hang-left-leg');
 hangLeftLeg.classList.add('invisible');
-gallowsWrapper.appendChild(hangLeftLeg);
+gallowsWrapper.append(hangLeftLeg);
+imagesVisible.push(hangLeftLeg);
 
 const hangRightLeg = document.createElement('img');
 hangRightLeg.src = 'images/right-leg.svg';
 hangRightLeg.classList.add('hang-right-leg');
 hangRightLeg.classList.add('invisible');
-gallowsWrapper.appendChild(hangRightLeg);
+gallowsWrapper.append(hangRightLeg);
+imagesVisible.push(hangRightLeg);
 
 const riddleWrapper = document.createElement('div');
 riddleWrapper.classList.add('riddle-wrapper');
-main.appendChild(riddleWrapper);
+main.append(riddleWrapper);
+
+const background = document.createElement('div');
+background.classList.add('background');
+background.classList.add('invisible');
+body.append(background);
 
 const riddles = [
   {
@@ -64,33 +78,33 @@ const riddles = [
   },
   {
     ordinal: 2,
-    question: 'мя',
-    answer: 'мя'
+    question: '1/4 часть круга',
+    answer: 'квадрант'
   },
   {
     ordinal: 3,
-    question: '3',
-    answer: '3'
+    question: 'Одна из отраслей изобразительного искусства, которую часто называют "искусством красивого письма"',
+    answer: 'каллиграфия'
   },
   {
     ordinal: 4,
-    question: '4',
-    answer: '4'
+    question: 'Парусный спорт в программе Олимпийских игр',
+    answer: 'регата'
   },
   {
     ordinal: 5,
-    question: '5',
-    answer: '5'
+    question: 'Известный вулкан в Италии',
+    answer: 'Везувий'
   },
   {
     ordinal: 6,
-    question: '6',
-    answer: '6'
+    question: 'Маленький, серенький, на слона похож. Кто это?',
+    answer: 'слонёнок'
   },
   {
     ordinal: 7,
-    question: '7',
-    answer: '7'
+    question: 'У него лапки, как у кота, и мордочка, как у кота, но это не кот. Кто это?',
+    answer: 'котёнок'
   },
   {
     ordinal: 8,
@@ -111,22 +125,26 @@ const riddles = [
 
 const questionAnswer = document.createElement('div');
 questionAnswer.classList.add('question-answer');
-riddleWrapper.appendChild(questionAnswer);
+riddleWrapper.append(questionAnswer);
 
 const question = document.createElement('p');
 question.classList.add('question');
-questionAnswer.appendChild(question);
+questionAnswer.append(question);
 
 const answer = document.createElement('p');
 answer.classList.add('answer');
-questionAnswer.appendChild(answer);
+questionAnswer.append(answer);
 
 let num = Math.ceil(Math.random() * 10);
+let riddleAnswer = '';
+let rightAnswer = '';
 
 riddles.forEach((riddle) => {
   if (riddle.ordinal === num) {
     question.textContent = riddle.question;
-    answer.textContent = riddle.answer;
+    answer.textContent = `${'__ '.repeat(riddle.answer.length - 1)}__`;
+    riddleAnswer = riddle.answer.toLowerCase();
+    rightAnswer = riddle.answer;
   }
 })
 
@@ -135,15 +153,63 @@ let scoreNum = 0;
 const score = document.createElement('p');
 score.classList.add('score');
 score.textContent = `Попытки: ${scoreNum}/6`;
-riddleWrapper.appendChild(score);
+riddleWrapper.append(score);
 
 const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
-riddleWrapper.appendChild(keyboard);
+riddleWrapper.append(keyboard);
 
 for (let i = 0; i < alphabet.length; i += 1) {
   let key = document.createElement('div');
   key.classList.add('key');
-  keyboard.appendChild(key);
+  keyboard.append(key);
   key.textContent = alphabet[i].toUpperCase();
 }
+
+const winMessage = 'Вы победили!';
+const loseMessage = `Увы, не повезло. Правильный ответ: ${rightAnswer}`;
+
+const keys = document.querySelectorAll('.key');
+const usedLetters = [];
+
+const chooseLetter = function(sym) {
+  let letter = sym.textContent.toLowerCase();
+  if (!usedLetters.includes(letter)) {
+    if (riddleAnswer.includes(letter)) {
+      let answerArray = answer.textContent.split(' ');
+      for (let i = 0; i < riddleAnswer.length; i += 1) {
+        if (riddleAnswer[i] === letter) {
+          answerArray[i] = letter.toUpperCase();
+          riddleAnswer = riddleAnswer.replace(letter, letter.toUpperCase());
+        }
+        answer.textContent = answerArray.join(' ');
+      }
+    } else {
+      scoreNum += 1;
+      imagesVisible[scoreNum].classList.remove('invisible');
+      score.textContent = `Попытки: ${scoreNum}/6`;
+      if (scoreNum >= 6) {
+        background.classList.remove('invisible');
+      }
+    }
+    sym.classList.add('key-inactive');
+    usedLetters.push(letter);
+    console.log(usedLetters);
+  }
+}
+
+keys.forEach((key) => {
+  key.addEventListener('click', () => {
+    chooseLetter(key);
+  })
+})
+
+window.addEventListener('keyup', (event) => {
+  if (alphabet.includes(event.key.toLowerCase()) && !usedLetters.includes(event.key)) {
+    keys.forEach(key => {
+      if (key.textContent.toLowerCase() === event.key.toLowerCase()) {
+        chooseLetter(key);
+      }
+    })
+  }
+})
