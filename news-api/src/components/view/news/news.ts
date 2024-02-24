8338945bacb0634a80e37bd7f9c1975eb584a54d
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-02-24 17:22:49 +0400

    feat: convert news file from javascript to typescript and fix problems

---
+++ news-api/src/components/view/news/news.ts
@@ -0,0 +1,40 @@
+import './news.css';
+import { Articles, Everything } from '../../../types/interfaces';
+
+class News {
+    draw(data: Articles[]) {
+        const news = data.length >= 10 ? data.filter((_item: Articles, idx: number) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = <HTMLTemplateElement>document.querySelector('#newsItemTemp');
+
+        news.forEach((item: Articles, idx: number): void => {
+            const newsClone = <HTMLTemplateElement>newsItemTemp.content.cloneNode(true);
+
+            if (idx % 2) newsClone.querySelector('.news__item')!.classList.add('alt');
+
+            const newsClonePhoto = <HTMLDivElement>newsClone.querySelector('.news__meta-photo');
+            newsClonePhoto.style.backgroundImage = `url(${
+                item.urlToImage || 'img/news_placeholder.jpg'
+            })`;
+            newsClone.querySelector('.news__meta-author')!.textContent = item.author || item.source.name;
+            newsClone.querySelector('.news__meta-date')!.textContent = item.publishedAt
+                .slice(0, 10)
+                .split('-')
+                .reverse()
+                .join('-');
+
+            newsClone.querySelector('.news__description-title')!.textContent = item.title;
+            newsClone.querySelector('.news__description-source')!.textContent = item.source.name;
+            newsClone.querySelector('.news__description-content')!.textContent = item.description;
+            newsClone.querySelector('.news__read-more a')!.setAttribute('href', item.url);
+
+            fragment.append(newsClone);
+        });
+
+        document.querySelector('.news')!.innerHTML = '';
+        document.querySelector('.news')!.appendChild(fragment);
+    }
+}
+
+export default News;
