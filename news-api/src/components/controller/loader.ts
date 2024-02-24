8c3335290bfd6219687155a2bca2cc71bf8cd326
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-02-24 19:21:54 +0400

    feat: convert loaders file from javascript to typescript and fix problems

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,19 +1,21 @@
 class Loader {
-    constructor(baseLink, options) {
+    private baseLink: string;
+    private options: object;
+    constructor(baseLink: string, options: object) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
+        { endpoint, options = {} }: {endpoint: string, options: object},
+        callback = (): void => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,18 +25,18 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
-        let url = `${this.baseLink}${endpoint}?`;
+    makeUrl(options: object, endpoint: string): string {
+        const urlOptions: object = { ...this.options, ...options };
+        let url: string = `${this.baseLink}${endpoint}?`;
 
-        Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
+        Object.entries(urlOptions).forEach(([key, value]: [key: string, value: string]): void => {
+            url += `${key}=${value}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: string, endpoint: string, callback: Function, options = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
