9914a7fa199aa16246afbe43f4a4d04de7a2f3d9
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-02-26 00:45:41 +0400

    feat: add switch for light-theme, but not add functional

--- news-api/src/index.ts
+++ news-api/src/index.ts
@@ -1,5 +1,19 @@
 import App from './components/app/app';
 import './global.css';
 
+const switchTheme = document.querySelector('.switch-label');
+const body = document.body;
+
+if (switchTheme) {
+    switchTheme.addEventListener('click', () => {
+        if (!body.classList.contains('light-theme')) {
+            console.log(true);
+            body.classList.add('light-theme');
+        } else {
+            body.classList.remove('light-theme');
+        }
+    });
+}
+
 const app = new App();
 app.start();
