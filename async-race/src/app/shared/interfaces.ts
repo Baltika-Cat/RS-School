b8a7d7fb99c2fd3d0990d72fb9ddcbcf15eec2ef
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-03-29 02:21:27 +0400

    feat: add interfaces file with interfaces Car and CarParams

---
+++ async-race/src/app/shared/interfaces.ts
@@ -0,0 +1,12 @@
+export interface Car {
+  name: string;
+  color: string;
+  id?: number;
+  status?: 'started' | 'stopped' |'drive';
+  carView?: HTMLObjectElement;
+}
+
+export interface CarParams {
+  velocity: number;
+  distance: number;
+}
