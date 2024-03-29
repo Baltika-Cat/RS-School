0d155db330fefe4821a67f79a05912f533ea5874
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-03-29 15:34:00 +0400

    feat: add class Controller for create and update cars

---
+++ async-race/src/app/garage/page-elements/controller.ts
@@ -0,0 +1,26 @@
+import { div, input } from '../../shared/tags';
+
+export class Controller {
+  controllerWrapper = div('controller-wrapper');
+  createCarArea = div('create-car-area', this.controllerWrapper);
+  createCarInput = input('input', 'text', this.createCarArea);
+  createCarColor = input('input-color', 'color', this.createCarArea);
+  createCarButton = div('button', this.createCarArea, 'CREATE');
+  updateCarArea = div('update-car-area', this.controllerWrapper);
+  updateCarInput = input('input', 'text', this.updateCarArea);
+  updateCarColor = input('input-color', 'color', this.updateCarArea);
+  updateCarButton = div('button', this.updateCarArea, 'UPDATE');
+  create = {
+    name: this.createCarInput,
+    color: this.createCarColor,
+    button: this.createCarButton,
+  }
+  update = {
+    name: this.updateCarInput,
+    color: this.updateCarColor,
+    button: this.updateCarButton,
+  }
+  constructor(garageWrapper: HTMLDivElement) {
+    garageWrapper.append(this.controllerWrapper);
+  }
+}
